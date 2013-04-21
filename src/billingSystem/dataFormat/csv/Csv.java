080939513b18488b6c2bdbe3dfb14503681e3c50
ma2dev <e164num@gmail.com>
2013-04-21 15:09:30 +0900

    add csv処理

--- src/billingSystem/dataFormat/csv/Csv.java
+++ src/billingSystem/dataFormat/csv/Csv.java
@@ -1,5 +1,34 @@
 package billingSystem.dataFormat.csv;
 
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Csv {
 
+	private CsvConfiguration config;
+	private List<List<Cell>> cellArray;
+
+	public Csv() {
+		config = new CsvConfiguration();
+		cellArray = new ArrayList<List<Cell>>();
+	}
+
+	public Csv(CsvConfiguration config) {
+		this.config = config;
+	}
+
+	public CsvConfiguration getConfiguration() {
+		return config;
+	}
+
+	public Cell getCell(int row, int column) {
+		List<Cell> line = cellArray.get(row);
+		Cell cell = line.get(column);
+		return cell;
+	}
+
+	public void readFrom(Reader reader, CsvConfiguration config) {
+
+	}
 }
---
+++ src/billingSystem/dataFormat/csv/CsvTest.java
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+package billingSystem.dataFormat.csv;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ma2dev
+ *
+ */
+public class CsvTest extends Csv {
+
+	/**
+	 * {@link billingSystem.dataFormat.csv.Csv#Csv()} のためのテスト・メソッド。
+	 */
+	@Test
+	public final void testCsv() {
+		fail("まだ実装されていません"); // TODO
+	}
+
+	/**
+	 * {@link billingSystem.dataFormat.csv.Csv#Csv(billingSystem.dataFormat.csv.CsvConfiguration)}
+	 * のためのテスト・メソッド。
+	 */
+	@Test
+	public final void testCsvCsvConfiguration() {
+		fail("まだ実装されていません"); // TODO
+	}
+
+}
