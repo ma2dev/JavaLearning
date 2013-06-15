bfe4cdad45bd2e5adf73f5028ccc2e9d5e3fb21d
ma2dev <e164num@gmail.com>
2013-06-16 03:25:45 +0900

    fix 不要なコメントの削除

--- src/billingSystem/billing/BillingCalculater.java
+++ src/billingSystem/billing/BillingCalculater.java
@@ -36,7 +36,6 @@ public class BillingCalculater {
 	 * @param serviceInformation
 	 *            サービス情報
 	 * @throws IOException
-	 * @throws
 	 */
 	public BillingCalculater(Configure configure, final IBillingPersonalInformation personalInformation,
 			final IBillingCallInformation callInformation, final IBillingServiceInformation serviceInformation)
