eb1f9bbaba7495f2fdeb35083216b643be875a1b
ma2dev <e164num@gmail.com>
2013-06-21 00:17:33 +0900

    update データ量の検証追加 todo データが空の場合の挙動

--- src/com/github/ma2dev/bcs/dataFormat/csv/CsvVerification.java
+++ src/com/github/ma2dev/bcs/dataFormat/csv/CsvVerification.java
@@ -1,5 +1,9 @@
 package com.github.ma2dev.bcs.dataFormat.csv;
 
+import java.util.List;
+
+import com.github.ma2dev.bcs.dataFormat.IData;
+
 /**
  * CSVデータの妥当性を検証する仕組みを提供します。<br>
  *
@@ -34,4 +38,180 @@ public class CsvVerification {
 		return false;
 	}
 
+	/**
+	 * 行数の検証をします。
+	 *
+	 * @param size
+	 *            行数
+	 * @return 指定された行数ある場合はtrueを、無い場合はfalseを返却します。
+	 */
+	public boolean isRowSize(int size) {
+		if (csv.getRowSize() == size) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 行数の検証をします。<br>
+	 * 指定した行について指定された行数より大きいことを検証します。
+	 *
+	 * @param size
+	 *            行数
+	 * @return 指定された行数*より大きい*場合はtrueを、以下の場合はfalseを返却します。
+	 */
+	public boolean isRowSizeMoreThan(int size) {
+		if (csv.getRowSize() > size) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 行数の検証をします。<br>
+	 * 指定した行について指定された行数*以上*であることを検証します。
+	 *
+	 * @param size
+	 *            行数
+	 * @return 指定された行数*以上*の場合はtrueを、より小さい場合はfalseを返却します。
+	 */
+	public boolean isRowSizeMoreThanOrEqual(int size) {
+		if (csv.getRowSize() >= size) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 列数の検証をします。<br>
+	 * 全ての行について指定された列数であることを検証します。<br>
+	 * なお、データが1行も無い場合に、0列あることを検証した場合にはtrueを返却します。
+	 *
+	 * @param size
+	 *            列数
+	 * @return 指定された行数ある場合はtrueを、無い場合はfalseを返却します。
+	 */
+	public boolean isColumnSize(int size) {
+		if (this.isRowSizeMoreThan(0) == true) {
+			for (int i = 0; i < csv.getRowSize(); i++) {
+				List<IData> line = csv.getCells(i);
+				if (line.size() != size) {
+					return false;
+				}
+			}
+			return true;
+		} else if (this.isRowSize(0) && size == 0) {
+			// データがないとき、列数0は真のためtrueを返却する。
+			return true;
+		}
+
+		// 行が無い場合
+		return false;
+	}
+
+	/**
+	 * 列数の検証をします。<br>
+	 * 指定した行について指定された列数であることを検証します。<br>
+	 * なお、指定した行が存在しない場合はfalseを返却します。<br>
+	 * このため、指定した行が存在しないとき、該当行に0列あることを検証した場合にはfalseを返却します。
+	 *
+	 * @param row
+	 *            行指定
+	 * @param size
+	 *            列数
+	 * @return 指定された行数ある場合はtrueを、無い場合はfalseを返却します。
+	 */
+	public boolean isColumnSize(int row, int size) {
+		if (csv.getRowSize() < row + 1) {
+			return false;
+		}
+
+		List<IData> line = csv.getCells(row);
+		if (line.size() != size) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 列数の検証をします。<br>
+	 * 全ての行について指定された列数*より大きい*ことを検証します。
+	 *
+	 * @param size
+	 *            列数
+	 * @return 指定された列数*より大きい*場合はtrueを、以下の場合はfalseを返却します。
+	 */
+	public boolean isColumnSizeMoreThan(int size) {
+		for (int i = 0; i < csv.getRowSize(); i++) {
+			List<IData> line = csv.getCells(i);
+			if (line.size() <= size) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 列数の検証をします。<br>
+	 * 指定した行について指定された列数*より大きい*ことを検証します。<br>
+	 * なお、指定した行が存在しない場合はfalseを返却します。
+	 *
+	 * @param row
+	 *            行指定
+	 * @param size
+	 *            列数
+	 * @return 指定された列数*より大きい*場合はtrueを、以下の場合はfalseを返却します。
+	 */
+	public boolean isColumnSizeMoreThan(int row, int size) {
+		if (csv.getRowSize() < row + 1) {
+			return false;
+		}
+
+		List<IData> line = csv.getCells(row);
+		if (line.size() <= size) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 列数の検証をします。<br>
+	 * 全ての行について指定された列数*以上*であることを検証します。
+	 *
+	 * @param size
+	 *            列数
+	 * @return 指定された列数*以上*の場合はtrueを、より小さい場合はfalseを返却します。
+	 */
+	public boolean isColumnSizeMoreThanOrEqual(int size) {
+		for (int i = 0; i < csv.getRowSize(); i++) {
+			List<IData> line = csv.getCells(i);
+			if (line.size() < size) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 列数の検証をします。<br>
+	 * 指定した行について指定された列数*以上*であることを検証します。<br>
+	 * なお、指定した行が存在しない場合はfalseを返却します。
+	 *
+	 * @param row
+	 *            行指定
+	 * @param size
+	 *            列数
+	 * @return 指定された列数*以上*の場合はtrueを、より小さい場合はfalseを返却します。
+	 */
+	public boolean isColumnSizeMoreThanOrEqual(int row, int size) {
+		if (csv.getRowSize() < row + 1) {
+			return false;
+		}
+
+		List<IData> line = csv.getCells(row);
+		if (line.size() < size) {
+			return false;
+		}
+		return true;
+	}
 }
--- test/com/github/ma2dev/bcs/dataFormat/csv/CsvVerificationTest.java
+++ test/com/github/ma2dev/bcs/dataFormat/csv/CsvVerificationTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class CsvVerificationTest {
 
 	@Test
-	public final void test有効データ保持有無の確認() {
+	public final void test有効データ保持有無の検証() {
 
 		Csv csv = new Csv();
 		CsvVerification verification = new CsvVerification(csv);
@@ -20,4 +20,187 @@ public class CsvVerificationTest {
 		assertThat(verification.isConstructed(), is(true));
 	}
 
+	@Test
+	public final void test行数の検証() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		assertThat(verification.isRowSize(-1), is(false));
+		assertThat(verification.isRowSize(0), is(true));
+		csv.setCell(0, 0, "ABC");
+		assertThat(verification.isRowSize(0), is(false));
+		assertThat(verification.isRowSize(1), is(true));
+		assertThat(verification.isRowSize(2), is(false));
+		csv.setCell(1, 0, "ABC");
+		assertThat(verification.isRowSize(1), is(false));
+		assertThat(verification.isRowSize(2), is(true));
+		assertThat(verification.isRowSize(3), is(false));
+	}
+
+	@Test
+	public final void test行数の検証_より大きい場合() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		assertThat(verification.isRowSizeMoreThan(-1), is(true));
+		assertThat(verification.isRowSizeMoreThan(0), is(false));
+		csv.setCell(0, 0, "ABC");
+		assertThat(verification.isRowSizeMoreThan(0), is(true));
+		assertThat(verification.isRowSizeMoreThan(1), is(false));
+		csv.setCell(1, 0, "ABC");
+		assertThat(verification.isRowSizeMoreThan(1), is(true));
+		assertThat(verification.isRowSizeMoreThan(2), is(false));
+	}
+
+	@Test
+	public final void test行数の検証_以上の場合() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		assertThat(verification.isRowSizeMoreThanOrEqual(-1), is(true));
+		assertThat(verification.isRowSizeMoreThanOrEqual(0), is(true));
+		assertThat(verification.isRowSizeMoreThanOrEqual(1), is(false));
+		csv.setCell(0, 0, "ABC");
+		assertThat(verification.isRowSizeMoreThanOrEqual(0), is(true));
+		assertThat(verification.isRowSizeMoreThanOrEqual(1), is(true));
+		assertThat(verification.isRowSizeMoreThanOrEqual(2), is(false));
+		csv.setCell(1, 0, "ABC");
+		assertThat(verification.isRowSizeMoreThanOrEqual(1), is(true));
+		assertThat(verification.isRowSizeMoreThanOrEqual(2), is(true));
+		assertThat(verification.isRowSizeMoreThanOrEqual(3), is(false));
+	}
+
+	@Test
+	public final void test列数の検証() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		assertThat(verification.isColumnSize(-1), is(false));
+		assertThat(verification.isColumnSize(0), is(true));
+		assertThat(verification.isColumnSize(1), is(false));
+
+		csv.setCell(0, 0, "x");
+		csv.setCell(0, 1, "y");
+		assertThat(verification.isColumnSize(2), is(true));
+		csv.setCell(1, 1, "z");
+		assertThat(verification.isColumnSize(2), is(true));
+		csv.setCell(2, 3, "a");
+		assertThat(verification.isColumnSize(2), is(false));
+
+		assertThat(verification.isColumnSize(-1), is(false));
+	}
+
+	@Test
+	public final void test指定行に対する列数の検証() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		assertThat(verification.isColumnSize(0, -1), is(false));
+		assertThat(verification.isColumnSize(0, 0), is(false));
+		assertThat(verification.isColumnSize(0, 1), is(false));
+
+
+		csv.setCell(0, 0, "x");
+		csv.setCell(0, 1, "y");
+		assertThat(verification.isColumnSize(0, 2), is(true)); // 0行目には2列分あるので
+		csv.setCell(1, 1, "z");
+		assertThat(verification.isColumnSize(1, 2), is(true)); // 1行目には2列分あるので
+		csv.setCell(2, 3, "a");
+		assertThat(verification.isColumnSize(2, 3), is(false));// 2行目には4列分あるが検証は3列分のため
+
+		assertThat(verification.isColumnSize(2, 4), is(true)); // 2行目には4列分あり検証は4列分のため
+		assertThat(verification.isColumnSize(3, 0), is(false));// 3行目にはデータが無いが3行目を検証しているため
+		csv.setCell(3, 0, "b");
+		assertThat(verification.isColumnSize(3, 0), is(false));// 3行目には1列分あり検証は0列分のため
+		assertThat(verification.isColumnSize(3, 1), is(true)); // 3行目には1列分あり検証は1列分のため
+	}
+
+	@Test
+	public final void test列数の検証_より大きい場合() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		// TODO データ未設定時
+
+		csv.setCell(0, 0, "x");
+		csv.setCell(0, 1, "y");
+		assertThat(verification.isColumnSizeMoreThan(2), is(false)); // 2列で2列よりも大きくはない
+		assertThat(verification.isColumnSizeMoreThan(1), is(true)); // 2列で1列よりも大きい
+		csv.setCell(1, 0, "z");
+		assertThat(verification.isColumnSizeMoreThan(1), is(false)); // 2行目が1列で1列よりも大きくはない
+		csv.setCell(1, 1, "a");
+		assertThat(verification.isColumnSizeMoreThan(1), is(true)); // 全てが2列で1列よりも大きい
+		csv.setCell(1, 2, "b");
+		assertThat(verification.isColumnSizeMoreThan(1), is(true)); // 全てが2列で1列よりも大きい
+		assertThat(verification.isColumnSizeMoreThan(2), is(false)); // 1行目が全てが2列で2列よりも大きくない
+	}
+
+	@Test
+	public final void test指定行に対する列数の検証_より大きい場合() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		// TODO データ未設定時
+
+		csv.setCell(0, 0, "x");
+		csv.setCell(0, 1, "y");
+		assertThat(verification.isColumnSizeMoreThan(0, 2), is(false)); // 検証対象は2列で2列よりも大きくはない
+		csv.setCell(1, 1, "z");
+		assertThat(verification.isColumnSizeMoreThan(1, 1), is(true)); // 検証対象は2列で1列よりも大きい
+		csv.setCell(2, 3, "a");
+		assertThat(verification.isColumnSizeMoreThan(2, 3), is(true));// 検証対象は4列分あり3列よりも大きい
+
+		assertThat(verification.isColumnSizeMoreThan(3, 0), is(false));// 3行目にはデータが無いが3行目を検証しているため
+		csv.setCell(3, 0, "b");
+		assertThat(verification.isColumnSizeMoreThan(3, 0), is(true));// 検証対象は1列分あり0列よりも大きい
+	}
+
+	@Test
+	public final void test列数の検証_以上の場合() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		// TODO データ未設定時
+
+		assertThat(verification.isColumnSizeMoreThanOrEqual(-1), is(true)); // 2列で-1列以上ある
+		csv.setCell(0, 0, "x");
+		assertThat(verification.isColumnSizeMoreThanOrEqual(-1), is(true)); // 2列で-1列以上ある
+
+		csv.setCell(0, 1, "y");
+		assertThat(verification.isColumnSizeMoreThanOrEqual(1), is(true)); // 2列で1列以上ある
+		assertThat(verification.isColumnSizeMoreThanOrEqual(2), is(true)); // 2列で2列以上ある
+		assertThat(verification.isColumnSizeMoreThanOrEqual(3), is(false)); // 2列で3列以上はない
+
+		csv.setCell(1, 0, "z");
+		csv.setCell(1, 1, "a");
+		assertThat(verification.isColumnSizeMoreThanOrEqual(2), is(true)); // 2列で2列以上ある
+
+		csv.setCell(1, 2, "b");
+		assertThat(verification.isColumnSizeMoreThanOrEqual(1), is(true)); // 2列で1列以上ある
+		assertThat(verification.isColumnSizeMoreThanOrEqual(2), is(true)); // 2列で2列以上ある
+		assertThat(verification.isColumnSizeMoreThanOrEqual(3), is(false)); // 2列で3列以上はない
+	}
+
+	@Test
+	public final void test指定行に対する列数の検証_以上の場合() {
+		Csv csv = new Csv();
+		CsvVerification verification = new CsvVerification(csv);
+
+		// TODO データ未設定時
+
+		csv.setCell(0, 0, "x");
+		csv.setCell(0, 1, "y");
+		assertThat(verification.isColumnSizeMoreThanOrEqual(0, 1), is(true)); // 検証対象は2列で1列以上
+		assertThat(verification.isColumnSizeMoreThanOrEqual(0, 2), is(true)); // 検証対象は2列で2列以上
+		assertThat(verification.isColumnSizeMoreThanOrEqual(0, 3), is(false)); // 検証対象は2列で3列以上ではない
+
+		csv.setCell(2, 3, "a");
+		assertThat(verification.isColumnSizeMoreThanOrEqual(2, 3), is(true));// 検証対象は4列分あり3列以上
+		assertThat(verification.isColumnSizeMoreThanOrEqual(2, 4), is(true));// 検証対象は4列分あり4列以上
+		assertThat(verification.isColumnSizeMoreThanOrEqual(2, 5), is(false));// 検証対象は4列分あり5列以上ではない
+
+		assertThat(verification.isColumnSizeMoreThanOrEqual(3, 0), is(false));// 3行目にはデータが無いが3行目を検証しているため
+		csv.setCell(3, 0, "b");
+		assertThat(verification.isColumnSizeMoreThanOrEqual(3, 1), is(true));// 検証対象は1列分あり1列以上
+	}
 }
