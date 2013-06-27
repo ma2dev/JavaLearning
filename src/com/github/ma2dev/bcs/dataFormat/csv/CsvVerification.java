3f57074d2caceac825c3cfae9e420637b82266df
ma2dev <e164num@gmail.com>
2013-06-27 22:15:15 +0900

    change CsvVerificationをstaticメソッドに変更(csvのオブジェクトを保持しておく必要が無いため)

--- src/com/github/ma2dev/bcs/dataFormat/csv/CsvVerification.java
+++ src/com/github/ma2dev/bcs/dataFormat/csv/CsvVerification.java
@@ -21,26 +21,16 @@ public class CsvVerification {
 	private static final String VERIFICATION_MATCH_NUMBER = "^[0-9]+$";
 	private static final String VERIFICATION_MATCH_ALPHABET_AND_NUMBER = "^[0-9A-Za-z]+$";
 
-	private Csv csv;
-
-	/**
-	 * コンストラクタ
-	 *
-	 * @param csv
-	 *            検証対象のcsvデータ
-	 */
-	public CsvVerification(Csv csv) {
-		this.csv = csv;
-	}
-
 	// nullチェック --------------------------------------------------------------
 	/**
 	 * 検証対象のcsvデータにデータが構築さているかを検証します。<br>
 	 * データの有無を確認するため、csvオブジェクトが単純にデータを保持しているかどうかを確認するものです。<br>
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @return データが一つでも設定されている場合はtrueを、データが一つも設定されていない場合はfalseを返却します。
 	 */
-	public boolean isConstructed() {
+	public static boolean isConstructed(Csv csv) {
 		if (csv.getRowSize() != 0) {
 			return true;
 		}
@@ -51,29 +41,16 @@ public class CsvVerification {
 
 	// 行数チェック --------------------------------------------------------------
 	/**
-	 * 行数の検証をします。
-	 *
-	 * @param size
-	 *            行数
-	 * @return 指定された行数ある場合はtrueを、無い場合はfalseを返却します。
-	 */
-	public boolean isRowSize(int size) {
-		if (csv.getRowSize() == size) {
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
 	 * 行数の検証をします。<br>
 	 * 指定された行数より大きいことを検証します。
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param size
 	 *            行数
 	 * @return 指定された行数*より大きい*場合はtrueを、以下の場合はfalseを返却します。
 	 */
-	public boolean isRowSizeMoreThan(int size) {
+	public static boolean isRowSizeMoreThan(Csv csv, int size) {
 		if (csv.getRowSize() > size) {
 			return true;
 		}
@@ -85,11 +62,13 @@ public class CsvVerification {
 	 * 行数の検証をします。<br>
 	 * 指定された行数*以上*であることを検証します。
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param size
 	 *            行数
 	 * @return 指定された行数*以上*の場合はtrueを、より小さい場合はfalseを返却します。
 	 */
-	public boolean isRowSizeMoreThanOrEqual(int size) {
+	public static boolean isRowSizeMoreThanOrEqual(Csv csv, int size) {
 		if (csv.getRowSize() >= size) {
 			return true;
 		}
@@ -100,121 +79,30 @@ public class CsvVerification {
 	 * 行数の検証をします。<br>
 	 * 指定された行数*以下*であることを検証します。
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param size
 	 *            行数
 	 * @return 指定された行数*以下*の場合はtrueを、より大きい場合はfalseを返却します。
 	 */
-	public boolean isRowSizeLessThanOrEqual(int size) {
-		return !(isRowSizeMoreThan(size));
+	public static boolean isRowSizeLessThanOrEqual(Csv csv, int size) {
+		return !(CsvVerification.isRowSizeMoreThan(csv, size));
 	}
 
 	// 列数チェック --------------------------------------------------------------
 	/**
 	 * 列数の検証をします。<br>
-	 * 全ての行について指定された列数であることを検証します。<br>
-	 * なお、検証対象のデータが1行も無い場合はfalseを返却します。
-	 *
-	 * @param size
-	 *            列数
-	 * @return 指定された行数ある場合はtrueを、無い場合はfalseを返却します。
-	 */
-	public boolean isColumnSize(int size) {
-		if (this.isConstructed() == false) {
-			// 1行もデータが無い場合
-			return false;
-		}
-
-		for (int i = 0; i < csv.getRowSize(); i++) {
-			List<IData> line = csv.getCells(i);
-			if (line.size() != size) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	/**
-	 * 列数の検証をします。<br>
-	 * 指定した行について指定された列数であることを検証します。<br>
-	 * なお、指定した行が存在しない場合はfalseを返却します。
-	 *
-	 * @param row
-	 *            行指定
-	 * @param size
-	 *            列数
-	 * @return 指定された行数ある場合はtrueを、無い場合はfalseを返却します。
-	 */
-	public boolean isColumnSize(int row, int size) {
-		if (csv.getRowSize() < row + 1) {
-			return false;
-		}
-
-		List<IData> line = csv.getCells(row);
-		if (line.size() != size) {
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * 列数の検証をします。<br>
-	 * 全ての行について指定された列数*より大きい*ことを検証します。<br>
-	 * なお、検証対象のデータが1行も無い場合はfalseを返却します。
-	 *
-	 * @param size
-	 *            列数
-	 * @return 指定された列数*より大きい*場合はtrueを、以下の場合はfalseを返却します。
-	 */
-	public boolean isColumnSizeMoreThan(int size) {
-		if (this.isConstructed() == false) {
-			// 1行もデータが無い場合
-			return false;
-		}
-
-		for (int i = 0; i < csv.getRowSize(); i++) {
-			List<IData> line = csv.getCells(i);
-			if (line.size() <= size) {
-				log.info("index: {}, expected: {}, actual: {}", i, size, line.size());
-				return false;
-			}
-		}
-		return true;
-	}
-
-	/**
-	 * 列数の検証をします。<br>
-	 * 指定した行について指定された列数*より大きい*ことを検証します。<br>
-	 * なお、指定した行が存在しない場合はfalseを返却します。
-	 *
-	 * @param row
-	 *            行指定
-	 * @param size
-	 *            列数
-	 * @return 指定された列数*より大きい*場合はtrueを、以下の場合はfalseを返却します。
-	 */
-	public boolean isColumnSizeMoreThan(int row, int size) {
-		if (csv.getRowSize() < row + 1) {
-			return false;
-		}
-
-		List<IData> line = csv.getCells(row);
-		if (line.size() <= size) {
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * 列数の検証をします。<br>
 	 * 全ての行について指定された列数*以上*であることを検証します。<br>
 	 * なお、検証対象のデータが1行も無い場合はfalseを返却します。
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param size
 	 *            列数
 	 * @return 指定された列数*以上*の場合はtrueを、より小さい場合はfalseを返却します。
 	 */
-	public boolean isColumnSizeMoreThanOrEqual(int size) {
-		if (this.isConstructed() == false) {
+	public static boolean isColumnSizeMoreThanOrEqual(Csv csv, int size) {
+		if (CsvVerification.isConstructed(csv) == false) {
 			// 1行もデータが無い場合
 			return false;
 		}
@@ -231,38 +119,17 @@ public class CsvVerification {
 
 	/**
 	 * 列数の検証をします。<br>
-	 * 指定した行について指定された列数*以上*であることを検証します。<br>
-	 * なお、指定した行が存在しない場合はfalseを返却します。
-	 *
-	 * @param row
-	 *            行指定
-	 * @param size
-	 *            列数
-	 * @return 指定された列数*以上*の場合はtrueを、より小さい場合はfalseを返却します。
-	 */
-	public boolean isColumnSizeMoreThanOrEqual(int row, int size) {
-		if (csv.getRowSize() < row + 1) {
-			return false;
-		}
-
-		List<IData> line = csv.getCells(row);
-		if (line.size() < size) {
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * 列数の検証をします。<br>
 	 * 全ての行について指定された列数*以下*であることを検証します。<br>
 	 * なお、検証対象のデータが1行も無い場合はfalseを返却します。
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param size
 	 *            列数
 	 * @return 指定された列数*以下*の場合はtrueを、より大きい場合はfalseを返却します。
 	 */
-	public boolean isColumnSizeLessThanOrEqual(int size) {
-		if (this.isConstructed() == false) {
+	public static boolean isColumnSizeLessThanOrEqual(Csv csv, int size) {
+		if (CsvVerification.isConstructed(csv) == false) {
 			// 1行もデータが無い場合
 			return false;
 		}
@@ -279,42 +146,14 @@ public class CsvVerification {
 
 	// 桁数チェック --------------------------------------------------------------
 	/**
-	 * 指定された特定情報の桁数をチェックします。<br>
-	 * 桁数は半角数字(バイト数)でのチェックとします。<br>
-	 * なお、指定した行列のデータが存在しない場合はfalseを返却します。
-	 *
-	 * @param row
-	 *            行
-	 * @param column
-	 *            列
-	 * @param number
-	 *            桁数
-	 * @return 指定された桁数の場合はtrueを、そうでない場合はfalseを返却します。
-	 */
-	public boolean isDigit(int row, int column, int byteNumber) {
-		// 行チェック
-		IData data = csv.getCell(row, column);
-		if (data == null) {
-			return false;
-		}
-
-		String str = (String) data.getData();
-		int length = str.getBytes().length; // バイト数取得
-		if (byteNumber != length) {
-			log.info("row: {}, column: {}, expected: {}, actual: {}", row, column, byteNumber, length);
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
 	 * 指定列のデータ桁数下限値を検証します。<br>
 	 * 検証桁数以上の場合にtrueを、検証桁数よりも小さい場合にfalseを返却します。<br>
 	 * 桁数は半角数字(バイト数)でのチェックとします。<br>
 	 * なお、必須時に指定した行列のデータが存在しない場合はfalseを返却します。<br>
 	 * また、検証対象のデータが1行も無い場合はfalseを返却します。
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param column
 	 *            列
 	 * @param digitLow
@@ -323,13 +162,13 @@ public class CsvVerification {
 	 *            必須(true)/非必須(false)を指定
 	 * @return
 	 */
-	public boolean isColumnDigitLower(int column, int digitLow, boolean must) {
-		if (this.isConstructed() == false) {
+	public static boolean isColumnDigitLower(Csv csv, int column, int digitLow, boolean must) {
+		if (CsvVerification.isConstructed(csv) == false) {
 			// 1行もデータが無い場合
 			return false;
 		}
 		if (must == true) {
-			if (isColumnMust(column) == false) {
+			if (isColumnMust(csv, column) == false) {
 				// 必須チェック
 				return false;
 			}
@@ -365,6 +204,8 @@ public class CsvVerification {
 	 * なお、指定した行列のデータが存在しない場合はfalseを返却します。<br>
 	 * また、検証対象のデータが1行も無い場合はfalseを返却します。
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param column
 	 *            列
 	 * @param digitUpper
@@ -373,13 +214,13 @@ public class CsvVerification {
 	 *            必須(true)/非必須(false)を指定
 	 * @return
 	 */
-	public boolean isColumnDigitUpper(int column, int digitUpper, boolean must) {
-		if (this.isConstructed() == false) {
+	public static boolean isColumnDigitUpper(Csv csv, int column, int digitUpper, boolean must) {
+		if (CsvVerification.isConstructed(csv) == false) {
 			// 1行もデータが無い場合
 			return false;
 		}
 		if (must == true) {
-			if (isColumnMust(column) == false) {
+			if (isColumnMust(csv, column) == false) {
 				// 必須チェック
 				return false;
 			}
@@ -415,12 +256,14 @@ public class CsvVerification {
 	 * 指定された列がnullもしくは空文字の場合はfalseを返却し、それ以外はtrueを返却します。<br>
 	 * なお、検証対象のデータが1行も無い場合はfalseを返却します。
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param column
 	 *            列
 	 * @return 指定された列がnullもしくは空文字の場合はfalseを返却し、それ以外はtrueを返却します。
 	 */
-	public boolean isColumnMust(int column) {
-		if (this.isConstructed() == false) {
+	public static boolean isColumnMust(Csv csv, int column) {
+		if (CsvVerification.isConstructed(csv) == false) {
 			// 1行もデータが無い場合
 			log.info("all data are null");
 			return false;
@@ -451,6 +294,8 @@ public class CsvVerification {
 	 * なお、検証対象のデータが1行も無い場合はfalseを返却します。
 	 *
 	 *
+	 * @param csv
+	 *            検証対象のcsvデータ
 	 * @param column
 	 *            列
 	 * @param typeAlphabet
@@ -464,13 +309,13 @@ public class CsvVerification {
 	 *            次の行の該当列の要素の検証を引き続き実施します。
 	 * @return 指定された列要素のデータ型が指定された型である場合はtrueを、そうで無い場合はfalseを返却します。
 	 */
-	public boolean isColumnType(int column, boolean typeAlphabet, boolean typeNumeric, boolean must) {
-		if (this.isConstructed() == false) {
+	public static boolean isColumnType(Csv csv, int column, boolean typeAlphabet, boolean typeNumeric, boolean must) {
+		if (CsvVerification.isConstructed(csv) == false) {
 			// 1行もデータが無い場合
 			return false;
 		}
 		if (must == true) {
-			if (isColumnMust(column) == false) {
+			if (isColumnMust(csv, column) == false) {
 				// 必須チェック
 				return false;
 			}
@@ -485,23 +330,23 @@ public class CsvVerification {
 			}
 			String s = (String) data.getData();
 			if (typeAlphabet == true && typeNumeric == false) {
-				if (isAlphabet(s) == false) {
+				if (CsvVerification.isAlphabet(s) == false) {
 					log.info("type not alphabet [{}]", s);
 					return false;
 				}
 			} else if (typeAlphabet == false && typeNumeric == true) {
-				if (isNumeric(s) == false) {
+				if (CsvVerification.isNumeric(s) == false) {
 					log.info("type not numeric [{}]", s);
 					return false;
 				}
 			} else if (typeAlphabet == true && typeNumeric == true) {
-				if (isAlphabetNumeric(s) == false) {
+				if (CsvVerification.isAlphabetNumeric(s) == false) {
 					log.info("type not alphabet and not numeric [{}]", s);
 					return false;
 				}
 			} else {
 				// 半角英数字で無い場合
-				if (isAlphabetNumeric(s)) {
+				if (CsvVerification.isAlphabetNumeric(s)) {
 					log.info("type alphabet or numeric [{}]", s);
 					return false;
 				}
@@ -517,7 +362,7 @@ public class CsvVerification {
 	 *            検証対象文字列
 	 * @return 半角英数字のみで構成されている場合はtrueを、そうで無い場合はfalseを返却します。
 	 */
-	private boolean isAlphabetNumeric(String s) {
+	private static boolean isAlphabetNumeric(String s) {
 		return s.matches(VERIFICATION_MATCH_ALPHABET_AND_NUMBER);
 	}
 
@@ -528,7 +373,7 @@ public class CsvVerification {
 	 *            検証対象文字列
 	 * @return 半角英字のみで構成されている場合はtrueを、そうで無い場合はfalseを返却します。
 	 */
-	private boolean isAlphabet(String s) {
+	private static boolean isAlphabet(String s) {
 		return s.matches(VERIFICATION_MATCH_ALPHABET);
 	}
 
@@ -539,7 +384,7 @@ public class CsvVerification {
 	 *            検証対象文字列
 	 * @return 半角数字のみで構成されている場合はtrueを、そうで無い場合はfalseを返却します。
 	 */
-	private boolean isNumeric(String s) {
+	private static boolean isNumeric(String s) {
 		return s.matches(VERIFICATION_MATCH_NUMBER);
 	}
 
